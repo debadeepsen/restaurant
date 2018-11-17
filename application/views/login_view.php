6bb921716a7b80252bb1bc10f72aee26fdea1036
Debadeep Sen <dsen@ncsu.edu>
2018-11-17 03:35:21 -0500

    Login screen

---
+++ application/views/login_view.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Please log in</title>
+	<meta charset="utf-8" />
+</head>
+<body>
+<form method="post" action="dashboard">
+    <div>Email:</div>
+    <div><input type="text" name="email"></input></div>
+    <div>Password:</div>
+    <div><input type="password" name="password"></input></div>
+    <div><input type="submit" name="submit"></input></div>
+</body>
+</html>
